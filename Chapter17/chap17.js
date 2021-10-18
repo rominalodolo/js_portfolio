41b034e2e8bd80033145d89a65c77c803fd6dacc
Romina <83961643+rominalodolo@users.noreply.github.com>
2021-10-18 14:36:30 +0200

    Add files via upload

--- Chapter17/chap17.js
+++ Chapter17/chap17.js
@@ -1,14 +1,5 @@
 var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-var weather = [
-  "Sunny",
-  "Partly Sunny",
-  "Partly Cloudy",
-  "Cloudy",
-  "Raining",
-  "Snowing",
-  "Thunderstorm",
-  "Foggy",
-];
+var weather = ["Sunny", "Partly Sunny", "Partly Cloudy", "Cloudy", "Raining", "Snowing", "Thunderstorm", "Foggy",];
 var maxTemp = 100;
 var minTemp = 0;
 
@@ -17,17 +8,7 @@ function generateWeather() {
     var weatherToday = weather[Math.floor(Math.random() * weather.length)];
     var tempToday = Math.floor(Math.random() * (maxTemp - minTemp) + minTemp);
     document.getElementById("5DayWeather").innerHTML +=
-      "<div id='" +
-      days[i] +
-      "' class='" +
-      weatherToday +
-      "'><b>Forecast for " +
-      days[i] +
-      ":</b><br><br>" +
-      weatherToday +
-      " and " +
-      tempToday +
-      " degrees.</div>";
+      "<div id='" + days[i] + "' class='" + weatherToday + "'><b>Forecast for " + days[i] + ":</b><br><br>" + weatherToday + " and " + tempToday + " degrees.</div>";
   }
 }
 
